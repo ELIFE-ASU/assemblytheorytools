e1343dc370fca9527efa8f8e44587a9d9c4bd3eb
Louie Slocombe <louies@hotmail.co.uk>
2025-08-04 16:50:14 -0700

    removed smiles dependence

--- assemblytheorytools/Rust_Ai.py
+++ assemblytheorytools/Rust_Ai.py
@@ -3,43 +3,43 @@ import subprocess
 import tempfile
 
 from rdkit import Chem
-from rdkit.Chem import AllChem
 
 
-def smiles_to_molfile(smiles, molfile_path):
-    mol = Chem.MolFromSmiles(smiles)
+def _input_helper(mol, molfile_path, add_hydrogens=True):
     if mol is None:
         return False
-    mol = Chem.AddHs(mol)
+    if add_hydrogens:
+        mol = Chem.AddHs(mol)
+    # Check for wildcard atoms (e.g., '*')
     has_wildcard = any(atom.GetSymbol() == '*' for atom in mol.GetAtoms())
-    if not has_wildcard:
-        ret = AllChem.EmbedMolecule(mol)
-        if ret != 0:
-            return False
+    # If there are wildcard atoms, we cannot write a valid molfile as not supported
+    if has_wildcard:
+        return False
     with open(molfile_path, 'w') as f:
         f.write(Chem.MolToMolBlock(mol))
     return True
 
 
-def calculate_rust_ai(smiles, exec_path=None, timeout=300):
+def calculate_rust_ai(mol, exec_path=None, timeout=300):
+    # Input validation
     if exec_path is None:
         exec_path = "/home/mshahjah/assembly-theory/target/release/assembly-theory"
-    if not smiles:
-        print("No SMILES provided.", flush=True)
+    if not mol:
+        print("No input provided.", flush=True)
         return -1
 
     with tempfile.NamedTemporaryFile(suffix='.mol', delete=True) as tmp_file:
         tmp_molfile = tmp_file.name
 
-        if not smiles_to_molfile(smiles, tmp_molfile):
-            print(f"Invalid SMILES: {smiles}", flush=True)
+        if not _input_helper(mol, tmp_molfile):
+            print(f"Invalid input", flush=True)
             return -1
 
         try:
             result = subprocess.run([exec_path, tmp_molfile], capture_output=True, text=True, timeout=timeout)
 
             if result.returncode != 0:
-                print(f"Error processing {smiles}: {result.stderr.strip()}", flush=True)
+                print(f"Error processing {mol}: {result.stderr.strip()}", flush=True)
                 return -1
 
             match = re.search(r'\b\d+\b', result.stdout)
