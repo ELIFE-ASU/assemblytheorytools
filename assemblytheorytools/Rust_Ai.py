4878367c961cc9e770954bcb801f004dfc1c6bd2
Louie Slocombe <louies@hotmail.co.uk>
2025-08-04 16:54:06 -0700

    added type annotations

--- assemblytheorytools/Rust_Ai.py
+++ assemblytheorytools/Rust_Ai.py
@@ -5,22 +5,29 @@ import tempfile
 from rdkit import Chem
 
 
-def _input_helper(mol, molfile_path, add_hydrogens=True):
+def _input_helper(mol: Chem.Mol,
+                  file_path: str,
+                  add_hydrogens: bool = True) -> bool:
     if mol is None:
         return False
     if add_hydrogens:
         mol = Chem.AddHs(mol)
+    else:
+        mol = Chem.RemoveHs(mol)
     # Check for wildcard atoms (e.g., '*')
     has_wildcard = any(atom.GetSymbol() == '*' for atom in mol.GetAtoms())
     # If there are wildcard atoms, we cannot write a valid molfile as not supported
     if has_wildcard:
         return False
-    with open(molfile_path, 'w') as f:
+    with open(file_path, 'w') as f:
         f.write(Chem.MolToMolBlock(mol))
     return True
 
 
-def calculate_rust_ai(mol, exec_path=None, timeout=300):
+def calculate_rust_ai(mol: Chem.Mol,
+                      exec_path: str | None = None,
+                      timeout: int = 300,
+                      add_hydrogens: bool = False) -> int:
     # Input validation
     if exec_path is None:
         exec_path = "/home/mshahjah/assembly-theory/target/release/assembly-theory"
@@ -31,7 +38,7 @@ def calculate_rust_ai(mol, exec_path=None, timeout=300):
     with tempfile.NamedTemporaryFile(suffix='.mol', delete=True) as tmp_file:
         tmp_molfile = tmp_file.name
 
-        if not _input_helper(mol, tmp_molfile):
+        if not _input_helper(mol, tmp_molfile, add_hydrogens=add_hydrogens):
             print(f"Invalid input", flush=True)
             return -1
 
@@ -43,7 +50,7 @@ def calculate_rust_ai(mol, exec_path=None, timeout=300):
                 return -1
 
             match = re.search(r'\b\d+\b', result.stdout)
-            return match.group(0) if match else result.stdout.strip()
+            return int(match.group(0) if match else result.stdout.strip())
         except Exception as e:
             print(f"Exception occurred: {e}", flush=True)
             return -1
