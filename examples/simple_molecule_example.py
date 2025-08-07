5638d0d9e0e5ce4b582c7d66df166d9bde77bac3
Louie Slocombe <louies@hotmail.co.uk>
2025-08-07 11:04:02 -0700

    removal of get_mol_pathway_to_smi #73

--- examples/simple_molecule_example.py
+++ examples/simple_molecule_example.py
@@ -5,11 +5,9 @@ if __name__ == "__main__":
     mol = att.smi_to_mol("c1ccccc1")
     # Calculate the assembly index
     ai, virt_obj, path = att.calculate_assembly_index(mol)
-    # Convert the pathway into smiles
-    smi_out = att.get_mol_pathway_to_smi(virt_obj)
 
     print(f"Assembly index: {ai}", flush=True)
-    print(f"virt_obj: {smi_out}", flush=True)
+    print(f"virt_obj: {virt_obj}", flush=True)
 
     # Convert to graph
     graph = att.mol_to_nx(mol)
