667f1c2840b0a4e8b1b672015b4627a20aab8335
Louie Slocombe <louies@hotmail.co.uk>
2025-08-27 16:58:13 -0700

    removed the deps #284

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ setup(
         'ipython>=8.30.0',
         'pyvis>=0.3.2',
         'CFG @ git+https://github.com/ELIFE-ASU/CFG.git',
-        'dagviz @ git+https://github.com/ELIFE-ASU/dagviz.git',
-        'cairosvg>=2.7.1',
     ],
     extras_require={
         'dev': [
