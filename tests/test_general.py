6d17efb625a8ac3213fb924e1899e6a816f0a879
Louie Slocombe <louies@hotmail.co.uk>
2025-09-15 17:52:54 -0700

    made test_cif #259

--- tests/test_general.py
+++ tests/test_general.py
@@ -408,59 +408,6 @@ def test_create_ionic_molecule():
     assert ai == 3
 
 
-# def test_cif_loading():
-#     print(flush=True)
-#     target_dir = "tests/data/cif_files/"
-#     dirs = att.file_list_all(os.path.expanduser(os.path.abspath(target_dir)))
-#     dirs.sort()
-#     print(dirs, flush=True)
-#     for file in dirs:
-#         print(file, flush=True)
-#         if 'Attakolite_0' in file:  # Attakolite_0 invalid spacegroup C 1 2/m 1
-#             continue
-#         if 'Wodginite_3' in file:  # Wodginite_3 invalid spacegroup C 1 2/c 1
-#             continue
-#         # input mol file
-#         atoms = att.read_cif_file(file)
-#         # tmp = ase.geometry.minkowski_reduce(atoms)
-#         # print(tmp)
-#
-#         import ase.build
-#         # ase.build.niggli_reduce(atoms)
-#         ase.build.tools.niggli_reduce(atoms)
-#         ase.build.tools.reduce_lattice(atoms)
-#
-#         view(atoms)
-#         tmp_file = file.split('.')[0] + ".mol"
-#         att.atoms_to_mol_file(atoms, file_name=tmp_file)
-#         atoms2 = read(tmp_file)
-#         view(atoms2)
-#         # os.remove(tmp_file)
-#         # check that the atoms are the same
-#         assert np.allclose(atoms.get_positions(), atoms2.get_positions(), rtol=1e-04, atol=1e-04)
-#         assert np.allclose(atoms.get_atomic_numbers(), atoms2.get_atomic_numbers())
-
-
-# def test_cif_ai():
-#     print(flush=True)
-#     target_dir = "tests/data/cif_files/"
-#     dirs = att.file_list_all(os.path.expanduser(os.path.abspath(target_dir)))
-#     file = dirs[0]
-#
-#     # input mol file
-#     atoms = att.read_cif_file(file)
-#     tmp_file = file.split('.')[0] + ".mol"
-#     att.atoms_to_mol_file(atoms, file_name=tmp_file)
-#     ai_mol, _, _ = att.calculate_assembly_index(tmp_file, joint_corr=False)
-#
-#     os.remove(tmp_file)
-#
-#     graph = att.atoms_to_nx(atoms)
-#     ai_graph, _, _ = att.calculate_assembly_index(graph, joint_corr=False)
-#
-#     assert ai_mol == ai_graph == 4
-
-
 # @pytest.mark.slow
 # def test_auto_compile():
 #     print(flush=True)
