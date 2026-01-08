a5cae582251858c028c373bc3085464947cc5022
Louie Slocombe <louies@hotmail.co.uk>
2026-01-08 16:20:04 -0700

    updated protocol #346

--- examples/protocols/1/protocol_1a.py
+++ examples/protocols/1/protocol_1a.py
@@ -3,14 +3,14 @@ import assemblytheorytools as att
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    mol_str = 6781
-    mol_str = 'diethyl phthalate'
-    smi = att.pubchem_id_to_smi(mol_str)
-    print(f"SMILES: {smi}", flush=True)
-    mol = att.pubchem_id_to_mol(mol_str)
-    graph = att.smi_to_nx('CCOC(=O)C1=CC=CC=C1C(=O)OCC', sanitize=True, add_hydrogens=True)
+    import os
+    os.environ["ASS_PATH"] = '../../../assemblytheorytools/precompiled/asscpp_combined_static_linux'
 
-    ai, virt_obj, pathway = att.calculate_assembly_index(graph, strip_hydrogen=True, timeout=1000.0)
+    smi = att.pubchem_name_to_smi('diethyl phthalate')
+    print(f"SMILES: {smi}", flush=True)
+    graph = att.smi_to_nx(smi, sanitize=True, add_hydrogens=True)
+
+    ai, virt_obj, pathway = att.calculate_assembly_index(graph, strip_hydrogen=True)
 
     # Print the calculated assembly index to the console
     print(f"Assembly index: {ai}", flush=True)
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     # Print the virtual objects to the console
     print(f"virt_obj: {virt_obj}", flush=True)
 
-    att.plot_pathway(pathway, show_icons=True, frame_on=True)
+    att.plot_pathway(pathway, show_icons=False, frame_on=True, plot_type='graph')
     plt.savefig("mol_pathway_example.svg")
     plt.show()
 
