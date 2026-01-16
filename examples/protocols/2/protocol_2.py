308648da560ca61dbae82bc32ecb4940264d0032
Louie Slocombe <louies@hotmail.co.uk>
2026-01-16 13:58:04 -0700

    updated protocol 2 #346

--- examples/protocols/2/protocol_2.py
+++ examples/protocols/2/protocol_2.py
@@ -1,24 +1,44 @@
-import assemblytheorytools as att
+import os
 
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    _, smis = att.sample_random_pubchem(100, seed=42)
+import assemblytheorytools as att
 
+import time as t
+
+if __name__ == "__main__":
+    t_0 = t.time()
+    n_sample = 10_000
+    sample_file = 'pubchem_sample.smi'
+
+    # Check if sample file exists
+    if not os.path.isfile(sample_file):
+        # If not, create it by sampling random PubChem molecules
+        _, smis = att.sample_random_pubchem(n_sample, seed=42)
+        t_1 = t.time()
+        print(f"Time to sample {n_sample} molecules: {t_1 - t_0:.1f} seconds", flush=True)
+
+        # Write the sampled SMILES to a file
+        with open(sample_file, 'w') as f:
+            for smi in smis:
+                f.write(f"{smi}\n")
+    else:
+        # If the file exists, read the SMILES from it
+        with open(sample_file, 'r') as f:
+            smis = [line.strip() for line in f.readlines()]
+
+    t_2 = t.time()
+    # Convert SMILES strings to RDKit molecule objects
     mols = [att.smi_to_mol(smi, sanitize=True, add_hydrogens=True) for smi in smis]
 
-    graphs = [att.smi_to_nx(smi, sanitize=True, add_hydrogens=True) for smi in smis]
+    # Get the molecular weights for each molecule
     mw = [att.molecular_weight(mol) for mol in mols]
 
-    # Calculate the assembly index, virtual objects, and pathway for the molecule
-    # The assembly index is a measure of molecular complexity
-    # Virtual objects represent intermediate structures in the assembly process
-    # The pathway describes the sequence of steps in the assembly process
-    ai, _, _ = att.calculate_assembly_parallel(graphs, settings={'strip_hydrogen': True, 'timeout': 10.0})
-
-    # Print the calculated assembly index to the console
-    print(f"Assembly index: {ai}", flush=True)
-
+    # Calculate the assembly index for the list of molecules
+    ai, _, _ = att.calculate_assembly_parallel(mols, settings={'strip_hydrogen': True,
+                                                               'timeout': 30.0})
+    t_3 = t.time()
+    print(f"Time to calculate assembly indices for molecules: {t_3 - t_2:.1f} seconds", flush=True)
 
     plt.scatter(mw, ai)
     plt.show()
