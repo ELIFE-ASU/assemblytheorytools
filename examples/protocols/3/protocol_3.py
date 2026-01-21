b62aa6df89c9bd30c9fd5c8f64cf59b65646541e
Louie Slocombe <louies@hotmail.co.uk>
2026-01-21 16:59:35 -0700

    updated protocol 3 #346

--- examples/protocols/3/protocol_3.py
+++ examples/protocols/3/protocol_3.py
@@ -1,27 +1,53 @@
+import matplotlib.pyplot as plt
+
 import assemblytheorytools as att
 
 if __name__ == "__main__":
-    # mols_str = ["papaverine",
-    #             "thebaine",
-    #             "codeine",
-    #             "morphine",
-    #             "diamorphine",
-    #             "fentanyl",
-    #             "methadone",
-    #             "remifentanil",
-    #             "salvinorin a",
-    #             "pethidine"]
-    #
-    # smis = [att.pubchem_name_to_smi(name) for name in mols_str]
-    # print(smis)
-    smis = ['COC1=C(C=C(C=C1)CC2=NC=CC3=CC(=C(C=C32)OC)OC)OC', 'CN1CC[C@]23[C@@H]4C(=CC=C2[C@H]1CC5=C3C(=C(C=C5)OC)O4)OC', 'CN1CC[C@]23[C@@H]4[C@H]1CC5=C2C(=C(C=C5)OC)O[C@H]3[C@H](C=C4)O', 'CN1CC[C@]23[C@@H]4[C@H]1CC5=C2C(=C(C=C5)O)O[C@H]3[C@H](C=C4)O', 'CC(=O)O[C@H]1C=C[C@H]2[C@H]3CC4=C5[C@]2([C@H]1OC5=C(C=C4)OC(=O)C)CCN3C', 'CCC(=O)N(C1CCN(CC1)CCC2=CC=CC=C2)C3=CC=CC=C3', 'CCC(=O)C(CC(C)N(C)C)(C1=CC=CC=C1)C2=CC=CC=C2', 'CCC(=O)N(C1=CC=CC=C1)C2(CCN(CC2)CCC(=O)OC)C(=O)OC', 'CC(=O)O[C@H]1C[C@H]([C@@]2(CC[C@H]3C(=O)O[C@@H](C[C@@]3([C@H]2C1=O)C)C4=COC=C4)C)C(=O)OC', 'CCOC(=O)C1(CCN(CC1)C)C2=CC=CC=C2']
+    # List of molecule names to convert to SMILES
+    mols_str = ["codeine",
+                "morphine",
+                "diamorphine",
+                "fentanyl"]
 
-    mols = [att.smi_to_mol(smi) for smi in smis]
-    # convert the SMILES strings to graphs
+    mols_str = ["codeine",
+                "diamorphine"]
+
+    timeout = 5.0 * 60.0
+
+    # Use the pubchem api to convert names to SMILES
+    smis = [att.pubchem_name_to_smi(name) for name in mols_str]
+
+    print(f"SMILES strings: {smis}", flush=True)
+
+    # Convert the SMILES strings to molecular graphs
     graphs = [att.smi_to_nx(smi) for smi in smis]
-    # Combine the graphs into a single graph
-    combined_graph = att.join_graphs(graphs)
-    ai, virt_obj, pathway = att.calculate_assembly_index(combined_graph, dir_code="/home/louie/skunkworks/assemblytheorytools/assemblytheorytools/precompiled/asscpp_combined_static_linux", strip_hydrogen=True, timeout=1.0)
 
-    print(ai)
+    ai_i = att.calculate_assembly_parallel(graphs, settings={'strip_hydrogen': True,
+                                                             'timeout': timeout})[0]
+    print(f"Individual assembly indices:", flush=True)
+    for i, name in enumerate(mols_str):
+        print(f"{name}: {ai_i[i]}", flush=True)
+
+    # Combine the graphs into a single graph for joint assembly calculation
+    combined_graph = att.join_graphs(graphs)
+    ai, virt_obj, pathway = att.calculate_assembly_index(combined_graph,
+                                                         strip_hydrogen=True,
+                                                         timeout=timeout)
+    print(f"Joint assembly index: {ai}", flush=True)
+
+    virt_obj = [att.nx_to_smi(vo, add_hydrogens=False) for vo in virt_obj]
+    print(f"Virtual objects in pathway: {virt_obj}", flush=True)
+
+    att.plot_pathway(pathway,
+                     frame_on=False,
+                     plot_type='mol', fig_size=(14, 7), layout_style='crossmin_long')
+    plt.savefig("mol_pathway_example.svg")
+    plt.savefig("mol_pathway_example.png", dpi=300)
+    plt.show()
+
+    att.plot_pathway(pathway,
+                     frame_on=False,
+                     plot_type='mol', fig_size=(14, 7), layout_style='sa')
+    plt.show()
+
     att.plot_digraph_metro(pathway, filename="metro_pathway_example")
