465be2e7a1557853ec5cf5721d4bf8285ee21783
Louie Slocombe <louies@hotmail.co.uk>
2026-01-21 14:44:31 -0700

    updated protocol 1 #346

--- examples/protocols/1/protocol_1a.py
+++ examples/protocols/1/protocol_1a.py
@@ -1,29 +1,20 @@
-import assemblytheorytools as att
-
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
-    import os
-    os.environ["ASS_PATH"] = '../../../assemblytheorytools/precompiled/asscpp_combined_static_linux'
+import assemblytheorytools as att
 
+if __name__ == "__main__":
     smi = att.pubchem_name_to_smi('diethyl phthalate')
     print(f"SMILES: {smi}", flush=True)
     graph = att.smi_to_nx(smi, sanitize=True, add_hydrogens=True)
-
     ai, virt_obj, pathway = att.calculate_assembly_index(graph, strip_hydrogen=True)
-
-    # Print the calculated assembly index to the console
     print(f"Assembly index: {ai}", flush=True)
 
-    # Convert the graphs in the pathway to smi, so I can use 'mol' plot type
-    for node in pathway.nodes():
-        node_graph = pathway.nodes[node]['vo']
-        node_smi = att.nx_to_smi(node_graph, add_hydrogens=False)
-        # print(f"Node {node}: {node_smi}")
-        pathway.nodes[node]['vo'] = node_smi
+    virt_obj = [att.nx_to_smi(vo, add_hydrogens=False) for vo in virt_obj]
+    print(f"Virtual objects in pathway: {virt_obj}", flush=True)
 
-    att.plot_pathway(pathway, show_icons=True, frame_on=False, plot_type='mol', fig_size=(14, 7), layout_style='crossmin_long')
+    att.plot_pathway(pathway,
+                     frame_on=False,
+                     plot_type='mol', fig_size=(14, 7), layout_style='crossmin_long')
     plt.savefig("mol_pathway_example.svg")
+    plt.savefig("mol_pathway_example.png", dpi=300)
     plt.show()
-
-    #att.plot_digraph_metro(pathway, filename="metro_pathway_example")
--- examples/protocols/1/protocol_1b.py
+++ examples/protocols/1/protocol_1b.py
@@ -2,23 +2,19 @@ import assemblytheorytools as att
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    # Define a single input string for analysis
     s_inpt = "gggfhhhvg"
 
-    # Calculate the string assembly index
     ai, virt_obj, pathway = att.calculate_string_assembly_index(
         s_inpt,
         directed=False,  # Treat as undirected assembly
         mode='mol'
     )
-
-    # Output the results
-    # Print the calculated assembly index, virtual objects, and assembly pathway to the console
     print(f"Assembly index: {ai}", flush=True)
-    print(f"virt_obj: {virt_obj}", flush=True)
-    print(f"path: {pathway}", flush=True)
+    print(f"Virtual objects in pathway: {virt_obj}", flush=True)
 
-
-    att.plot_pathway(pathway, show_icons=True, frame_on=True, fig_size=(14, 7), plot_type='string', layout_style = 'crossmin', node_color='white') #'crossmin', 'crossmin_long', 'sa'
+    att.plot_pathway(pathway,
+                     plot_type='string',
+                     layout_style='crossmin')
     plt.savefig("str_pathway_example.svg")
+    plt.savefig("str_pathway_example.png", dpi=300)
     plt.show()
